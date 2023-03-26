b3580b76fbb4b17df422e51c40d6583052eb761f
Sohel Nadaf <sohelnadafb4u@gmail.com>
2023-03-26 20:25:44 +0530

    project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(name="cencus-income",
+      version="0.0.1",
+      author="sohel nadaf",
+      author_email="sohelnadafb4u@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy"]
+
+
+      )
